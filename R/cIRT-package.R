006f6d467df79912a19feeeaf3925f600a654352
James Balamuta <balamut2@illinois.edu>
2016-04-13 09:28:10 +0000

    version 1.2.0

--- R/cIRT-package.R
+++ R/cIRT-package.R
@@ -3,13 +3,12 @@
 #' \tabular{ll}{
 #' Package: \tab cIRT\cr
 #' Type: \tab Package\cr
-#' Version: \tab 1.1.0\cr
-#' Date: \tab 2015-12-28\cr
+#' Version: \tab 1.2.0\cr
+#' Date: \tab 2016-04-12\cr
 #' License: \tab GPL-3\cr
 #' }
 #'
 #' @name cIRT_package
-#' @aliases cirt
 #' @docType package
 #' @title Item Response Theory Models with Guided Choice
 #' @seealso \code{\link{cIRT}}, \code{\link{TwoPLChoicemcmc}}, \code{\link{probitHLM}}, \code{\link{direct_sum}},  \code{\link{center_matrix}},
